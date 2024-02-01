d404ffd7ee81536e781069af15f5ce8ac5a64736
darandagomez <arandagomez01@gmail.com>
2024-02-01 18:22:14 +0100

    AndroidStudio_Inicio
    hola

---
+++ app/src/main/java/com/example/syncplay/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.syncplay;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botonRegistro,botonInicioSesion;
+    private EditText editTextDireccion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botonRegistro = (Button) findViewById(R.id.buttonRegistrarse);
+        botonInicioSesion = (Button) findViewById(R.id.buttonIniciarSesion);
+        editTextDireccion = (EditText) findViewById(R.id.editTextCorreoElectronico);
+
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrarse();
+            }
+        });
+
+        botonInicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarSesion();
+            }
+        });
+
+    }
+
+    private void registrarse(){
+        if(editTextDireccion.getText().length()<1){
+            alerta("No has introducido ningún correo");
+        }else{
+            String email = editTextDireccion.getText().toString();
+            String regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]+$";
+            if(email.matches(regex)){
+                Toast.makeText(this, "Accediendo al registro", Toast.LENGTH_SHORT).show();
+
+                Intent actividadRegistro = new Intent(this, VentanaRegistro.class);
+                actividadRegistro.putExtra("correo",String.valueOf(editTextDireccion.getText()));
+                startActivity(actividadRegistro);
+            }else{
+                alerta("Ese correo no existe");
+            }
+
+        }
+    }
+
+    private void iniciarSesion(){
+
+    }
+
+    private void alerta(String problema){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Establecemos el título del diálogo
+        builder.setTitle("¡¡Atención!!");
+        builder.setMessage(problema);
+        builder.setPositiveButton("Aceptar", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
---
+++ app/src/main/java/com/example/syncplay/VentanaRegistro.java
@@ -0,0 +1,95 @@
+package com.example.syncplay;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VentanaRegistro extends AppCompatActivity {
+
+    private Spinner spinerSexo;
+    private EditText editTextNombreRegistro,editTextCorreoRegistro,editTextContrasena,editTextTelefonoRegistro,editTextUsuarioRegistro;
+    private Button botonRegistro;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private String correo="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ventana_registro);
+
+
+        Bundle bundle = getIntent().getExtras();
+        correo = bundle.getString("correo");
+
+        editTextNombreRegistro = (EditText) findViewById(R.id.editTextNombreRegistro);
+        editTextCorreoRegistro = (EditText) findViewById(R.id.editTextCorreoRegistro);
+        editTextContrasena = (EditText) findViewById(R.id.editTextContrasena);
+        editTextTelefonoRegistro = (EditText) findViewById(R.id.editTextTelefonoRegistro);
+        editTextUsuarioRegistro = (EditText) findViewById(R.id.editTextUsuario);
+        botonRegistro = (Button) findViewById(R.id.buttonEnviar);
+
+        editTextCorreoRegistro.setText(correo);
+
+        editTextNombreRegistro.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                InputMethodManager im = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                im.hideSoftInputFromWindow(view.getWindowToken(),0);
+            }
+        });
+
+
+
+        botonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                annadirRegistro();
+            }
+        });
+
+
+
+
+
+
+    }
+
+    synchronized private void annadirRegistro(){
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("hola", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("adios", "Error adding document", e);
+                    }
+                });
+    }
+
+}
