b7e799215ad06defcbf63f9243d56e6cc5cee5ae
Pablo Murillo <pablo.murillo.retuerta@gmail.com>
2024-04-08 10:04:16 +0200

    Fondo animado login

--- app/src/main/java/com/example/syncplay/MainActivity.java
+++ app/src/main/java/com/example/syncplay/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.syncplay;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+
+public class MainActivity extends AppCompatActivity {
 
     private Button botonRegistro,botonInicioSesion;
     private EditText editTextDireccion;
-
     private TextView textviewRegistro;
 
     @Override
@@ -25,11 +27,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Obtener una referencia a la vista personalizada
-        AnimatedShapesView animatedShapesView = findViewById(R.id.animatedShapesView);
-
-        // Iniciar la animación
-        animatedShapesView.startAnimation();
+        RelativeLayout layout = findViewById(R.id.mainlayout);
+        AnimationDrawable fondo = (AnimationDrawable) layout.getBackground();
+        fondo.setEnterFadeDuration(2500);
+        fondo.setExitFadeDuration(5000);
+        fondo.start();
 
         //botonRegistro = (Button) findViewById(R.id.buttonRegistrarse);
         botonInicioSesion = (Button) findViewById(R.id.buttonIniciarSesion);
